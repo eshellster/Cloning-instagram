2c1073091e3e53e2d3e4a13f17f702d1004df351
eshell <eshellster@gmail.com>
2018-08-24 17:37:52 +0900

    update

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = "images"
+
 urlpatterns = [
-    url(
-        regex=r'^all/$',
-        view=views.ListAllImages.as_view(),
+    path(
+        "all/",
+        view=views.ListAllImages.as_view,
         name='all_images'
-    )
+    ),
 ]
